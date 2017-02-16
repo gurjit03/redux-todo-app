bcd4531cdf2dea57e082435c4b4a444374680c0f
gurjit03 <gurjitmehta@gmail.com>
2017-02-16 15:50:25 +0530

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');  
+var {Router, Route, IndexRoute, hashHistory} = require('react-router');
+
+// Load the foundation css for the component
+require('style!css!foundation-sites/dist/css/foundation.min.css')
+$(document).foundation();
+
+// App CSS
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <h1>Final Boilerplate application</h1>,
+  document.getElementById('app')
+);
